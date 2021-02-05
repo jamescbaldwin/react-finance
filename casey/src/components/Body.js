bbdcecb6dcb328ba141640c5acd0b66639ede732
Casey Overton <overtonc1@gmail.com>
2021-02-05 01:24:50 -0500

    added material ui, not rendering icons, but everything else is working

---
+++ casey/src/components/Body.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import "../style/style.css"
+
+export class Body extends Component {
+  render() {
+    return (
+  <div class="container">
+    <div class="row">
+    </div>
+  </div>
+    )
+  }
+};
+
+
+
+
+
+
+  export default Body;
