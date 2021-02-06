75e8551441a4b8c58bed286956db2d784bbe11b1
nihalwill <66322270+nihalwill@users.noreply.github.com>
2021-02-06 13:28:22 -0500

    API Key to run multiple requests

--- nihal/src/utils/API.js
+++ nihal/src/utils/API.js
@@ -7,7 +7,7 @@ let API = {
             "url":"https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete",
             "params":{"q":searchString, "region":"US"},
             "headers": {
-                "x-rapidapi-key": "dd74b83b8emsha204c262aaccdbbp1e7099jsnd652157dfa85",
+                "x-rapidapi-key": "f1567ee71emsh6ebf2858e9db922p1be8a0jsnc09115c90856",
 	            "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
 	            "useQueryString": true
                 }});
