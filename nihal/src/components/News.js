5aa05a7f952a2183f65f63c4efaca3989c5a2504
nihalwill <66322270+nihalwill@users.noreply.github.com>
2021-02-09 18:38:26 -0500

    Update News.js

--- nihal/src/components/News.js
+++ nihal/src/components/News.js
@@ -1,22 +1,14 @@
 
-var unirest = require("unirest");
+import React from "react";
 
-var req = unirest("GET", "https://apidojo-yahoo-finance-v1.p.rapidapi.com/news/v2/get-details");
+function News (props) {
+    return (
+        <div className="container">
+            <h3>{props.title}</h3>
+            {/* <p>{props.link}</p>
+            <p>{props.publisher}</p> */}
+        </div>
+    )
+}
 
-req.query({
-	"uuid": "9803606d-a324-3864-83a8-2bd621e6ccbd",
-	"region": "US"
-});
-
-req.headers({
-	"x-rapidapi-key": "f1567ee71emsh6ebf2858e9db922p1be8a0jsnc09115c90856",
-	"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
-	"useQueryString": true
-});
-
-
-req.end(function (res) {
-	if (res.error) throw new Error(res.error);
-
-	console.log(res.body);
-});
+export default News;
