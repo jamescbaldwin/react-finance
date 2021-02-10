9f22e8bd2177e81cd6cdfaf14b2350ad3d8994d6
Cadertea <cadertea@ncsu.edu>
2021-02-09 20:07:30 -0500

    added slider code

---
+++ chris/js.js
@@ -0,0 +1,18 @@
+
+    var swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 30,
+        stretch: 0,
+        depth: 500,
+        modifier: 1,
+        slideShadows: true,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+ 
