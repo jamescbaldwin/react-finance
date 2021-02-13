84ed62af3c06b885293020aeb11bbd79d083c829
Cadertea <cadertea@ncsu.edu>
2021-02-13 10:26:37 -0500

    pushing cardUi code with classes in it

--- chris/CardUI.jsx
+++ chris/CardUI.jsx
@@ -2,6 +2,46 @@ import React from 'react'
 
 const Card = props => {
     return(
+        <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+
+<div class="example">
+    <div class="card">
+        <div class="card-image"> </div>
+            <div class="card-text">
+                 <span class="date"> 4 days ago</span> 
+                 <h2>BB</h2>
+                 <p>Company Information: </p>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi, quam. Nostrum aspernatur
+                        fugiat mollitia quod soluta voluptates nesciunt aliquid officiis minima dicta porro ipsa
+                        repellat commodi, deleniti expedita possimus architecto?</p>
+                       
+            </div>
+            
+                <div class="card-stats">
+                    <div class="stat">
+                        <div class="value">4m</div>
+                        <div class="type">read</div>
+                    </div>
+                    <div class="stat border">
+                        <div class="value">5123</div>
+                        <div class="type">views</div>
+                    </div>
+                    <div class="stat">
+                        <div class="value">32</div>
+                        <div class="type">comments</div>
+                    </div>
+                </div>
+            
+        </div>
+</div>
+
+</div>
+</div>
+</div>
+
+
 
     );
     
