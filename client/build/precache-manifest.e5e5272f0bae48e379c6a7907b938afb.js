72cd6a19cec91c9d4a4175eb2f7473f9e8b4ae93
harold parada <haroldparada20@gmail.com>
2021-03-09 01:26:52 -0500

    added build

---
+++ client/build/precache-manifest.e5e5272f0bae48e379c6a7907b938afb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "211f8f85b6c6023715ab",
+    "url": "/static/css/main.63d9292b.chunk.css"
+  },
+  {
+    "revision": "211f8f85b6c6023715ab",
+    "url": "/static/js/main.58da9956.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "11bef0028b76517723b0",
+    "url": "/static/css/2.2f1ed38b.chunk.css"
+  },
+  {
+    "revision": "11bef0028b76517723b0",
+    "url": "/static/js/2.d8b6cce9.chunk.js"
+  },
+  {
+    "revision": "e2b6db6c590a75e389157b9ad0971121",
+    "url": "/static/media/Screenshot (178).e2b6db6c.png"
+  },
+  {
+    "revision": "5d5840718081ecb3d103ff89a7f66382",
+    "url": "/index.html"
+  }
+];
